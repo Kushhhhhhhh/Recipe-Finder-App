432e14ecf6de6196ae63d736944cf8c4119aea48
kush sharma <kush.73888@gmail.com>
2024-06-25 11:54:18 +0530

    FavoritePage Completed

--- src/pages/FavoritePage.jsx
+++ src/pages/FavoritePage.jsx
@@ -1,20 +1,26 @@
+import RecipeCard from "../components/RecipeCard"
+import { getRandomColor } from "../lib/utils"
 
 const FavoritePage = () => {
-  const fav = false;
+
+  const favorite = JSON.parse(localStorage.getItem("favorites")) || [];
+
   return (
     <div className="bg-[#faf9fb] flex-1 p-10 min-h-screen"> 
       <div className="max-w-screen-lg mx-auto">
          <p className="font-bold text-3xl md:text-5xl my-4">My Favorites</p>
 
-         {!fav && (
+         {favorite.length === 0 && (
           <div className="h-[80vh] flex flex-col items-center gap-4">
             <img src="/404.svg" alt="error-img" className="h-3/4"/>
           </div>
          )}
 
-         {fav && (
+         {favorite && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <RecipeCard />
+            {favorite.map((recipe) => (
+              <RecipeCard key={recipe.label} recipe={recipe} {...getRandomColor()} />
+            ))}
           </div>
          )}
          
