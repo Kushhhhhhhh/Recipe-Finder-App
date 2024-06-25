291007e263a4e73bde17092aa744ac17621b33c6
kush sharma <kush.73888@gmail.com>
2024-06-25 11:54:59 +0530

    Recipe Card Completed

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,31 +1,76 @@
 import { Soup, Heart, HeartPulse } from "lucide-react"
+import { useState } from "react"
 
-const RecipeCard = () => {
+const getTwoValues = (arr) => {
+    return [arr[0], arr[1]];
+};
+
+const RecipeCard = ({ recipe, bg, badge }) => {
+
+const healthLabels = getTwoValues(recipe.healthLabels);
+const [isFavorite, setIsFavorite] = useState(localStorage.getItem("favorites")?.includes(recipe.label));
+
+const addRecipeToFavortie = () => {
+  let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+  const isRecipeAlreadyFavorite = favorites.some(
+    (favorite) => favorite.label === recipe.label
+  )
+
+  if (isRecipeAlreadyFavorite) {
+     favorites = favorites.filter(
+       (favorite) => favorite.label !== recipe.label
+     )
+     setIsFavorite(false);
+  } else {
+    favorites.push(recipe);
+    setIsFavorite(true);
+  }
+
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+
+};
+
     return (
-        <div className="flex flex-col rounded-md bg-[#212121] overflow-hidden p-3 relative">
-            <a href="" className="relative h-32">
-                <img src="/1.jpg" alt="reciper-img" className="rounded-md w-full h-full object-cover cursor-pointer" />
+        <div className={`flex flex-col rounded-md ${bg} overflow-hidden p-3 relative`}>
+            <a 
+            href={`https://www.youtube.com/results?search_query=${recipe.label} recipe`} 
+            target="_blank"
+            className="relative h-32">
+              <div className="skeleton absolute inset-0" />
+                <img src={recipe.image} alt="recipe-image" className="rounded-md w-full h-full object-cover cursor-pointer opacity-0 transition-opacity duration-500"
+                onLoad={(e) => {
+                  e.currentTarget.style.opacity = 1;
+                  e.currentTarget.previousElementSibling.style.display = "none";
+                }}
+                />
                 <div className="absolute bottom-2 left-2 bg-white rounded-full p-1 cursor-pointer flex items-center gap-1 text-sm">
-                    <Soup size={16} /> 4 Servings
+                    <Soup size={16} /> {recipe.yield} Servings
                 </div>
-                <div className="absolute top-2 right-2 bg-white rounded-full p-1 cursor-pointer flex items-center gap-1 text-sm">
-                    <Heart size={20} className="hover:text-red-500 hover:fill-red-500" />
+                <div 
+                className="absolute top-2 right-2 bg-white rounded-full p-1 cursor-pointer flex items-center gap-1 text-sm"
+                onClick={(e) => {
+                  e.preventDefault();
+                  addRecipeToFavortie();
+                }}
+                >
+                    {!isFavorite && <Heart size={20} className='hover:fill-red-500 hover:text-red-500' />}
+					{isFavorite && <Heart size={20} className='fill-red-500 text-red-500' />}
                 </div>
             </a>
-            <div className="flex mt-1 text-slate-200">
-                <p className="font-bold tracking-wide">Roasted Chicken</p>
+            <div className="flex mt-1 text-slate-800">
+                <p className="font-bold tracking-wide">{recipe.label}</p>
             </div>
-            <p className="my-2 text-slate-400">Turkish Kitchen</p>
-            <div className="flex gap-2 mt-auto">
-                <div className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md">
-                    <HeartPulse size={16} />
-                    <span className="text-sm tracking-tighter font-semibold">Gluten-free</span>
-                </div>
-                <div className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md">
-                    <HeartPulse size={16} />
-                    <span className="text-sm tracking-tighter font-semibold">Heart-healthy</span>
-                </div>
-            </div>
+            <p className="my-2 text-slate-500">
+                {recipe.cuisineType[0].charAt(0).toUpperCase() + recipe.cuisineType[0].slice(1)} Kitchen
+            </p>
+            <div className='flex gap-2 mt-auto'>
+				{healthLabels.map((label, idx) => (
+					<div key={idx} className={`flex gap-1 items-center p-1 rounded-md ${badge}`}>
+						<HeartPulse size={16} />
+						<span className='text-sm tracking-tighter font-semibold'>{label}</span>
+					</div>
+				))}
+			</div>
         </div>
     )
 }
